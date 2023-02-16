7266614f939eddb6cf6500b75a19ff6b10cecf11
WesBourkeMusic <wesbourkeavl@gmail.com>
2023-02-15 21:41:53 -0500

    just keeping it pushing. No matter how short the practice is, I'm doing every day!

--- src/main/java/com/in28minutes/springboot/myFirstWebApp/hello/SayHelloController.java
+++ src/main/java/com/in28minutes/springboot/myFirstWebApp/hello/SayHelloController.java
@@ -6,12 +6,30 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SayHelloController {
+
     //say-hello => Hello what are you learning to day.
     //http://localhost:8080/say-hello
     @RequestMapping("say-hello")
     @ResponseBody
     public String sayHello() {
+
         return "Hello! What are you learning today?";
+    }
+
+    @RequestMapping("say-hello-html")
+    @ResponseBody
+    public String sayHelloHtml() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title> My first HTML Page</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("My first html page with body");
+        sb.append("</body>");
+        sb.append("</html>");
+
+        return sb.toString();
     }
 
 }
